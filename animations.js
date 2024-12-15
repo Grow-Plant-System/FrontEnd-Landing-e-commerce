c3cbba9df1bd8e0d2fce50df90101d93597ba710
nehuen villavicencio <villavicencionehuen@gmail.com>
2024-12-15 15:49:28 -0300

    FULL PICADO AGREGUE QUE LA ANIMACION de la planta 2 se active cuando el scroll sea mayor a 0 como la navbar sino no se ve centrado y puto toc

--- animations.js
+++ animations.js
@@ -1,72 +1,19 @@
-// const navbar = document.querySelector('header');
-// let navbarHeight = navbar.offsetHeight;
+const navbar = document.querySelector('header');
+let navbarHeight = navbar.offsetHeight;
 
 
-// function handleScroll() {
-//     if (window.scrollY > 0) {
-//         navbar.style.transform = 'translateY(0)';
-//     } else {
-//         navbar.style.transform = 'translateY(-100%)';
-//     }
-// }
+function handleScroll() {
+    if (window.scrollY > 0) {
+        navbar.style.transform = 'translateY(0)';
+        document.querySelector('.plantHero2').classList.add('fadeIn')
+    } else {
+        navbar.style.transform = 'translateY(-100%)';
+    }
+}
 
 
-// window.addEventListener('scroll', handleScroll);
+window.addEventListener('scroll', handleScroll);
 
-// const commonRevealOptions = {
-//     reset: false, 
-// };
-
-
-// const animations = [
-    // { selector: '.subtitleHero, .step-text, .FAQSection-title', className: 'swingIn' },
-
-// ];
-
-// animations.forEach(animation => {
-//     ScrollReveal().reveal(animation.selector, {
-//         ...commonRevealOptions,
-//         beforeReveal: (el) => {
-//             el.classList.add(animation.className);
-//         },
-//         beforeReset: (el) => {
-//             // para el pop ap
-//             if (animation.selector.includes('againPopUpEmailFinal-text')) {
-//                 el.classList.remove('focusOut');
-//             }
-//         }
-//     });
-// });
-
-
-// const navbar = document.querySelector('header');
-// let navbarHeight = navbar.offsetHeight;
-
-// // Función que maneja la visibilidad de la navbar
-// function handleScroll() {
-//     if (window.scrollY > 0) {
-//         navbar.style.transform = 'translateY(0)';
-//     } else {
-//         navbar.style.transform = 'translateY(-100%)';
-//     }
-// }
-
-// window.addEventListener('scroll', handleScroll);
-
-// // Implementación del IntersectionObserver para animaciones al hacer scroll
-// const observer = new IntersectionObserver((entries) => {
-//     entries.forEach(entry => {
-//         if (entry.isIntersecting) {
-//             // Agrega la clase de animación cuando el elemento entra en el viewport
-//             entry.target.classList.add(entry.target.dataset.animation); 
-//         }
-//     });
-// }, {
-//     threshold: 0.1  // Umbral de visibilidad (cuando el 20% del elemento es visible)
-// });
-
-// // Selecciona todos los elementos que tienen el atributo data-animation
-// document.querySelectorAll('[data-animation]').forEach(el => observer.observe(el));
 
 // Configuración del Intersection Observer
 const observerOptions = {
